844e8ee1674631f24a350297d574dbefae5bdeb0
matthewpdias <matthewpdias@gmail.com>
2016-09-26 11:50:18 -0700

    added tests, no bugs found yet

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
 class FieldTest : public ::testing::Test
 {
 	protected:
@@ -18,7 +18,27 @@ class FieldTest : public ::testing::Test
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+TEST(FieldTest, outOfBounds)
+{
+  Field minefield;
+  ASSERT_ANY_THROW(minefield.get(11,11));
+}
+
+TEST(FieldTest, unsafe)
+{
+  Field minefield;
+  minefield.placeMine(4,5);
+  ASSERT_EQ(false, minefield.isSafe(4,5));
+}
+
+TEST(FieldTest, safe)
+{
+  Field minefield;
+  minefield.placeMine(4,5);
+  ASSERT_EQ(EMPTY_HIDDEN, minefield.get(4,4));
 }
